6350d84ce4bcc03cafe2bf55d716cb64b7fddc9c
Muhammad Hanzala <hanzalaqurreshi740@gmail.com>
2025-09-10 20:53:58 +0500

    Third changes

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,21 +1,17 @@
 import speech_recognition as sr
+import streamlit as st
 
 def speech_to_text():
-    # Convert microphone input into urdu text 
     r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Speak ...")
-        r.adjust_for_ambient_noise(source,duration=1)
-        try:
-            audio_text = r.listen(source,timeout=20)
-            print("Recording Finished")
-            spoken_text = r.recognize_google(audio_text)
-            print("You said ",spoken_text)
-            return spoken_text
-        except sr.UnknownValueError:
-            print("I could not understand the voice")
-        except sr.RequestError:
-            print("Could not connect with google speech service")
-        except sr.WaitTimeoutError:
-            print("Timeout, Please Speak Again")
+    
+    try:
+        with sr.Microphone() as source:
+            print("Speak ...")
+            r.adjust_for_ambient_noise(source, duration=1)
+            audio = r.listen(source, timeout=5)
+            
+        text = r.recognize_google(audio)
+        return text
+    except Exception as e:
+        st.error(f"Audio recording error: {e}")
         return None
